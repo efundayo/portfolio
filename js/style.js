c4a41f423da6cc801856475102dd07b8ce9139c5
imosudi <imosudi@gmail.com>
2017-07-08 11:51:05 +0100

    dynamic year for copyright

---
+++ js/style.js
@@ -0,0 +1,41 @@
+
+       
+$(document).ready(function() {
+    $(".contentPost").delay(7000).fadeIn(10000);
+});
+ 
+
+
+
+       
+$(document).ready(function() {
+    $(".contentPost1").delay(9000).fadeIn(10000);
+});
+ 
+
+       
+$(document).ready(function() {
+    $(".contentPost2").delay(11000).fadeIn(10000);
+});
+ 
+
+ 
+$(document).ready(function() {
+    $(".contentPost3").delay(13000).fadeIn(10000);
+});
+ 
+
+
+       
+$(document).ready(function() {
+    $(".contentPost4").delay(15000).fadeIn(10000);
+});
+ 
+
+
+$(document).ready(function() {
+    $(".contentPost5").delay(17000).fadeIn(1000);
+});
+
+
+
